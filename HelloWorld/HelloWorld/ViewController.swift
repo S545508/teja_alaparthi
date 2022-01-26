6b6777c427cf104bf3e8265ddd0ed3fd6b05a618
teja alaparthi <tejaalaparthi4@gmail.com>
2022-01-25 18:08:16 -0600

    edited

---
+++ HelloWorld/HelloWorld/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Alaparthi,Teja on 1/20/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
